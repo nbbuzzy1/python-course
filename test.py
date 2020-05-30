bd6bb9bdd17c3680652a50a557e2728d566526c9
nbbuzzy1 <40919504+nbbuzzy1@users.noreply.github.com>
2020-05-30 12:52:26 -0400

    list methods part one.

--- test.py
+++ test.py
@@ -1,8 +1,26 @@
-#Matrix
-matrix = [
-    [1, 2, 3],
-    [2, 4, 6],
-    [7, 8, 9]
-]
+#List methods
 
-print(matrix[0][1])
+basket = [1, 2, 3, 4, 5]
+
+#adding
+new_basket = basket.append(100)
+print(new_basket)
+print(basket)
+
+#insert
+basket.insert(3, 100)
+print(basket)
+
+#extend
+basket.extend([50])
+print(basket)
+
+#removing
+test = basket.pop()
+print(test)
+
+basket.remove(2)
+print(basket)
+
+basket.clear()
+print(basket)
