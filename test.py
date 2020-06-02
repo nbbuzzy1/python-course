66b3ce694bfeca2d429e06729da51d46ce8d5bbd
nbbuzzy1 <40919504+nbbuzzy1@users.noreply.github.com>
2020-06-02 16:08:30 -0400

    Tuples.

--- test.py
+++ test.py
@@ -1,28 +1,10 @@
-#Dictionary
+#Tuple
 
-dictionary = {
-    'a': 1,
-    'b': 2
-}
+my_tuple = (1, 2, 3, 4, 5)
 
-print(dictionary['b'])
+new_tuple = my_tuple[:]
+print(new_tuple)
 
-# Keys
+print(new_tuple.index(5))
 
-dictionary = {
-    123: [1, 2, 3],
-    True: 'hello'
-}
-
-# Methods
-
-print(dictionary.get(123))
-print(dictionary.get(123, 'hi'))
-print(dictionary.get(123)) # these all return the same value
-
-name = dict(name = 'nick', age = 12)
-print(name)
-
-print(123 in dictionary)
-print('hello' in dictionary.values())
-print(dictionary.items())
+print(my_tuple.count(1))
