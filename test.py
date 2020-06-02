faa764b1c90756e0e5cfec017901abb50137c26d
nbbuzzy1 <40919504+nbbuzzy1@users.noreply.github.com>
2020-06-02 06:29:29 -0400

    Range and reversing.

--- test.py
+++ test.py
@@ -2,25 +2,7 @@
 
 basket = [1, 2, 3, 4, 5]
 
-#adding
-new_basket = basket.append(100)
-print(new_basket)
+modified_basket = basket[::-1]
 print(basket)
 
-#insert
-basket.insert(3, 100)
-print(basket)
-
-#extend
-basket.extend([50])
-print(basket)
-
-#removing
-test = basket.pop()
-print(test)
-
-basket.remove(2)
-print(basket)
-
-basket.clear()
-print(basket)
+print(list(range(1, 100)))
