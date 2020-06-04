daaf6aac31b0e4926b694cc489974dc7b9836407
nbbuzzy1 <40919504+nbbuzzy1@users.noreply.github.com>
2020-06-04 15:24:08 -0400

    Conditional logic intro.

--- test.py
+++ test.py
@@ -1,40 +1,12 @@
-#Set
+#conditional logic
+is_old = False
+is_licensed = True
 
-my_set = {1, 2, 3, 4, 5}
-print(my_set)
+if is_old and is_licensed:
+    print('you are old enough to drive')
+elif is_licensed:
+    print('how did you get your license??')
+else:
+    print('you are not old enough to drive')
 
-my_set.add(10)
-print(my_set)
-
-my_list = [1, 2, 3, 4, 4, 5]
-print(set(my_list))
-
-new_set = my_set.copy()
-print(new_set)
-
-your_set = {4, 5, 6, 7, 8, 9}
-
-# difference
-my_set.difference(your_set)
-
-# discard
-my_set.discard(5)
-
-# difference_update
-my_set.difference_update(your_set)
-print(my_set)
-
-# intersection
-my_set.intersection(your_set)
-
-# isdisjoint
-my_set.isdisjoint(your_set)
-
-# issubset
-my_set.issubset(your_set)
-
-# issuperset
-my_set.issuperset(your_set)
-
-# union
-my_set.union(your_set)
+print('check check')
