a98bb90c5f8d193cd3cda6ea1db4b50c6ce4ca66
nbbuzzy1 <40919504+nbbuzzy1@users.noreply.github.com>
2020-06-03 21:45:33 -0400

    Sets!.

--- test.py
+++ test.py
@@ -1,10 +1,40 @@
-#Tuple
+#Set
 
-my_tuple = (1, 2, 3, 4, 5)
+my_set = {1, 2, 3, 4, 5}
+print(my_set)
 
-new_tuple = my_tuple[:]
-print(new_tuple)
+my_set.add(10)
+print(my_set)
 
-print(new_tuple.index(5))
+my_list = [1, 2, 3, 4, 4, 5]
+print(set(my_list))
 
-print(my_tuple.count(1))
+new_set = my_set.copy()
+print(new_set)
+
+your_set = {4, 5, 6, 7, 8, 9}
+
+# difference
+my_set.difference(your_set)
+
+# discard
+my_set.discard(5)
+
+# difference_update
+my_set.difference_update(your_set)
+print(my_set)
+
+# intersection
+my_set.intersection(your_set)
+
+# isdisjoint
+my_set.isdisjoint(your_set)
+
+# issubset
+my_set.issubset(your_set)
+
+# issuperset
+my_set.issuperset(your_set)
+
+# union
+my_set.union(your_set)
