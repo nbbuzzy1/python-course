7bf0fb5410d4ef8365a8a2d94c3eb79b4c9a2b0d
nbbuzzy1 <40919504+nbbuzzy1@users.noreply.github.com>
2020-06-05 20:21:04 -0400

    While loops.

---
+++ Basics/while-loops.py
@@ -0,0 +1,28 @@
+# while loops
+
+i = 0
+
+while i < 50:
+    print(i)
+    i += 1
+else:
+    print('done with all the work')
+
+i = 0
+my_list = [1, 2, 3]
+
+while i < len(my_list):
+    print(i)
+    i += 1
+
+while True:
+    response = input('say something: ')
+    if (response == 'bye'):
+        break
+
+for item in my_list:
+    pass
+
+for item in my_list:
+    continue
+    print('hi')
